2ad3b53dd9a5ecf1b5b1232abe0a70154e8efdfd
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-04-01 15:01:45 +0200

    Adding AgeCalculator and ConvertorToTimeZone

---
+++ src/main/java/ua/skillup/AgeCalculator.java
@@ -0,0 +1,21 @@
+package ua.skillup;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class AgeCalculator {
+    public static int calculateAge(LocalDate dateOfBirth) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Input your date of birth in the format: 'yyyy-MM-dd'");
+        System.out.println("For example: '1990-01-01'");
+
+        Scanner scanner = new Scanner(System.in);
+        String dateOfBirth = scanner.nextLine();
+
+        System.out.println("Your age is: " + calculateAge(LocalDate.parse(dateOfBirth)));
+    }
+}
---
+++ src/test/java/ua/skillup/ConvertorToTimeZoneTest.java
@@ -0,0 +1,23 @@
+package ua.skillup;
+
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Scanner;
+
+import static org.testng.Assert.assertEquals;
+
+public class ConvertorToTimeZoneTest {
+    @Test
+    public void testConvertToTimezone() {
+        ZonedDateTime dateTime1 = ZonedDateTime.of(2020, 1, 1, 0, 0, 0, 0, ZoneId.of("Etc/GMT+0"));
+        ZonedDateTime converted = ConvertorToTimeZone.convertToTimezone(dateTime1, "Etc/GMT+1");
+
+        LocalDateTime expected = dateTime1.minusHours(1).toLocalDateTime();
+        LocalDateTime actual = converted.toLocalDateTime();
+
+        assertEquals(actual, expected);
+    }
+}
