f142f06b6c7dd844d16a189b5536fc37dd16eee4
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-04-01 20:55:06 +0200

    Removed redundant name field

--- src/main/java/ua/skillup/ZodiacSign.java
+++ src/main/java/ua/skillup/ZodiacSign.java
@@ -4,27 +4,25 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public enum ZodiacSign {
-    ARIES("Aries", 21, 3, 20, 4),
-    TAURUS("Taurus", 21, 4, 21, 5),
-    GEMINI("Gemini", 22, 5, 21, 6),
-    CANCER("Cancer", 22, 6, 22, 7),
-    LEO("Leo", 23, 7, 23, 8),
-    VIRGO("Virgo", 24, 8, 23, 9),
-    LIBRA("Libra", 24, 9, 23, 10),
-    SCORPIO("Scorpio", 24, 10, 22, 11),
-    SAGITTARIUS("Sagittarius", 23, 11, 21, 12),
-    CAPRICORN("Capricorn", 22, 12, 20, 1),
-    AQUARIUS("Aquarius", 21, 1, 19, 2),
-    PISCES("Pisces", 20, 2, 20, 3);
+    ARIES(21, 3, 20, 4),
+    TAURUS(21, 4, 21, 5),
+    GEMINI(22, 5, 21, 6),
+    CANCER(22, 6, 22, 7),
+    LEO(23, 7, 23, 8),
+    VIRGO(24, 8, 23, 9),
+    LIBRA(24, 9, 23, 10),
+    SCORPIO(24, 10, 22, 11),
+    SAGITTARIUS(23, 11, 21, 12),
+    CAPRICORN(22, 12, 20, 1),
+    AQUARIUS(21, 1, 19, 2),
+    PISCES(20, 2, 20, 3);
 
-    private final String name;
     private final int startDay;
     private final int startMonth;
     private final int endDay;
     private final int endMonth;
 
-    ZodiacSign(String name, int startDay, int startMonth, int endDay, int endMonth) {
-        this.name = name;
+    ZodiacSign(int startDay, int startMonth, int endDay, int endMonth) {
         this.startDay = startDay;
         this.startMonth = startMonth;
         this.endDay = endDay;
@@ -50,7 +48,7 @@ public enum ZodiacSign {
         LocalDate dateOfBirth = LocalDate.parse(dateOfBirthStr);
         ZodiacSign zodiacSign = ZodiacSign.getZodiacSign(dateOfBirth.getDayOfMonth(), dateOfBirth.getMonthValue());
 
-        if(zodiacSign != null) {
+        if (zodiacSign != null) {
             System.out.println("Your zodiac sign is: " + zodiacSign.name());
         } else {
             System.out.println("Invalid date of birth");
