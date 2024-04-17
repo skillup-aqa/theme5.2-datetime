c00f4e70052ce66d4cbcbf06b3f8ccf681eb57b8
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-17 09:32:41 -0400

    Resolving Zodiac Sign

--- src/main/java/ua/skillup/ZodiacSign.java
+++ src/main/java/ua/skillup/ZodiacSign.java
@@ -4,21 +4,38 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public enum ZodiacSign {
-    ARIES,
-    TAURUS,
-    GEMINI,
-    CANCER,
-    LEO,
-    VIRGO,
-    LIBRA,
-    SCORPIO,
-    SAGITTARIUS,
-    CAPRICORN,
-    AQUARIUS,
-    PISCES;
+    ARIES(21, 3, 20, 4),
+    TAURUS(21, 4, 21, 5),
+    GEMINI(22, 5, 21, 6),
+    CANCER(22, 6, 22, 7),
+    LEO(23, 7, 23, 8),
+    VIRGO(24, 8, 23, 9),
+    LIBRA(24, 9, 23, 10),
+    SCORPIO(24, 10, 22, 11),
+    SAGITTARIUS(23, 11, 21, 12),
+    CAPRICORN(22, 12, 20, 1),
+    AQUARIUS(21, 1, 19, 2),
+    PISCES(20, 2, 20, 3);
+
+    private final int beginDay;
+    private final int beginMonth;
+    private final int andDay;
+    private final int andMonth;
+
+    ZodiacSign(int beginDay, int beginMonth, int andDay, int andMonth) {
+        this.beginDay = beginDay;
+        this.beginMonth = beginMonth;
+        this.andDay = andDay;
+        this.andMonth = andMonth;
+    }
 
     public static ZodiacSign getZodiacSign(int day, int month) {
-        // TODO: implement this method
+        for (ZodiacSign sign : ZodiacSign.values()) {
+            if ((month == sign.beginMonth && day >= sign.beginDay)
+                    || (month == sign.andMonth && day <= sign.andDay)) {
+                return sign;
+            }
+        }
         return null;
     }
 
@@ -32,7 +49,7 @@ public enum ZodiacSign {
         LocalDate dateOfBirth = LocalDate.parse(dateOfBirthStr);
         ZodiacSign zodiacSign = ZodiacSign.getZodiacSign(dateOfBirth.getDayOfMonth(), dateOfBirth.getMonthValue());
 
-        if(zodiacSign != null) {
+        if (zodiacSign != null) {
             System.out.println("Your zodiac sign is: " + zodiacSign.name());
         } else {
             System.out.println("Invalid date of birth");
