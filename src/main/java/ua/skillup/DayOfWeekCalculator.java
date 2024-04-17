99ed16d1e12b9b363d4f7723fd337d9541323e86
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-16 22:46:52 -0400

    Resolving Day Of Week Calculator

--- src/main/java/ua/skillup/DayOfWeekCalculator.java
+++ src/main/java/ua/skillup/DayOfWeekCalculator.java
@@ -1,16 +1,28 @@
 package ua.skillup;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class DayOfWeekCalculator {
     public static String calculateDayOfWeek(int year, int month, int day) {
-        // TODO: implement this method
-        return null;
+        LocalDate date = LocalDate.of(year, month, day);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        return dayOfWeek.toString();
     }
 
     public static void printFirstDayOfWeekAndLastDayOfWeek() {
-        // TODO: implement this method
+        LocalDate today = LocalDate.now();
+        LocalDate firstDay = today.withDayOfMonth(1);
+        LocalDate lastDay = today.withDayOfMonth(today.lengthOfMonth());
+        System.out.println("First day of this month is: " + firstDay.getDayOfWeek().name());
+        System.out.println("Last day of this month is: " + lastDay.getDayOfWeek().name());
+
+
     }
 
     public static void main(String[] args) {
         printFirstDayOfWeekAndLastDayOfWeek();
+        System.out.println(calculateDayOfWeek(2024, 04, 16));
     }
 }
